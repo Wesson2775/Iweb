2fcf62a31432f79fee75c767429cabdae0c9483f
Wesson2775 <118877138+Wesson2775@users.noreply.github.com>
2025-07-19 21:51:15 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+window.siteConfig = {
+  siteName: "musifei.live",
+  tags: [
+    { name: "我的博客", url: "https://zor666.me" },
+    { name: "个人导航", url: "https://myssrhub.eu.org" },
+    { name: "热点汇总" }
+  ],
+  socialLinks: [
+    { icon: "fa-github", url: "https://github.com/" },
+    { icon: "fa-tv", url: "https://zor666.me" },
+    { icon: "fa-envelope", url: "mailto:your@email.com" },
+    { icon: "fa-qq", url: "https://wpa.qq.com/msgrd?v=3&uin=你的QQ号&site=qq&menu=yes" }
+  ]
+}; 
